69f5a090746fa53e6d385f0f05708756d2091d35
Enma2005 <enma.romero@alumnos.uvigo.es>
2025-09-22 11:19:19 +0200

    version 3 h

--- ET1_alumnograduacion_infotest.js
+++ ET1_alumnograduacion_infotest.js
@@ -13,7 +13,7 @@ el mensaje de respuesta asociado al resultado. (en el mismo idioma para todos lo
 
 //DEFINICIONES DE TESTS PARA LOGIN
 let alumnograduacion_def_tests = [
-    //SECCION CAMPOS LOGIN
+    //LOGIN
     ['alumnograduacion', 'alumnograduacion_login', 1, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_login_vacio_KO', 'Complete el campo'],
     ['alumnograduacion', 'alumnograduacion_login', 2, 'Validar que el campo este compuesto por alfabeticos sin ñ ni espacios ni acentos', 'ADD', 'alumnosgraduacion_login_formato_KO', 'No se admiten ni ñ, ni espacios, ni acentos.'],
     ['alumnograduacion', 'alumnograduacion_login', 3, 'Validar que el campo tenga un minimo de 4 caracteres', 'ADD', 'alumnograduacion_login_min_size_KO', 'Mínimo 4 caracteres.'],
@@ -48,6 +48,7 @@ let alumnograduacion_def_tests = [
     ['alumnograduacion', 'alumnograduacion_titulacion', 23, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'], 
 
     //DNI
+
     ['alumnograduacion', 'alumnograduacion_dni', 24, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_dni_vacio_KO', 'Complete el campo'],
     //HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA LOLOLOLOLOLOLLOLOLOLOLOLOLOLOL??????????????
     ['alumnograduacion', 'alumnograduacion_dni', 25, 'Validar que el campo tenga 8 numero y y una letra valida para los numeros','ADD', 'alumnograduacion_dni_formato_KO', 'El formato debe ser 8 dígitos y una letra mayúscula que corresponda al numero.'],
@@ -55,11 +56,36 @@ let alumnograduacion_def_tests = [
     ['alumnograduacion', 'alumnograduacion_dni', 27, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'],
 
     //TELEFONO
-    
+    ['alumnograduacion', 'alumnograduacion_telefono', 28, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_telefono_vacio_KO', 'Complete el campo'],
+    ['alumnograduacion', 'alumnograduacion_telefono', 29, 'Validar que el campo tenga un maximo de 9 caracteres', 'ADD', 'alumnograduacion_telefono_max_size_KO', 'Máximo 9 caracteres.'],   
+    ['alumnograduacion', 'alumnograduacion_telefono', 30, 'Validar que el campo este compuesto por numeros', 'ADD', 'alumnograduacion_telefono_formato_KO', 'Solo se admiten números.'],
+    ['alumnograduacion', 'alumnograduacion_telefono', 31, 'Validar que el campo tenga un minimo de 9 caracteres', 'ADD', 'alumnograduacion_telefono_min_size_KO', 'Mínimo 9 caracteres.'],
+    ['alumnograduacion', 'alumnograduacion_telefono', 32, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'],
 
+    //DIRECCION
+    ['alumnograduacion', 'alumnograduacion_direccion', 33, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_direccion_vacio_KO', 'Complete el campo'],
+    ['alumnograduacion', 'alumnograduacion_direccion', 34, 'Validar que el campo este compuesto por alfabeticos con ñ, espacios y acentos permitidos', 'ADD', 'alumnograduacion_direccion_formato_KO', 'No se admiten caracteres que no sean espacios, ñ o acentos.'],
+    //no especifica el minimo de direccion en el documento lo dejo o quito?
+    ['alumnograduacion', 'alumnograduacion_direccion', 35, 'Validar que el campo tenga un minimo de 2 caracteres', 'ADD', 'alumnograduacion_direccion_min_size_KO', 'Mínimo 2 caracteres.'],
+    ['alumnograduacion', 'alumnograduacion_direccion', 36, 'Validar que el campo tenga un maximo de 100 caracteres', 'ADD', 'alumnograduacion_direccion_max_size_KO', 'Máximo 100 caracteres.'],   
+    ['alumnograduacion', 'alumnograduacion_direccion', 37, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'], 
 
+    //EMAIL
+    ['alumnograduacion', 'alumnograduacion_email', 38, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_email_vacio_KO', 'Complete el campo'],
+    ['alumnograduacion', 'alumnograduacion_email', 39, 'Validar que el campo tenga un maximo de 40 caracteres', 'ADD', 'alumnograduacion_email_max_size_KO', 'Máximo 40 caracteres.'],   
+    //volvemos a lo mismo los minimos los considero o no?
+    ['alumnograduacion', 'alumnograduacion_email', 40, 'Validar que el campo este compuesto por un formato de email valido', 'ADD', 'alumnograduacion_email_formato_KO', 'El formato debe ser email válido.'],
+    ['alumnograduacion', 'alumnograduacion_email', 41, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'],  
 
-
+    //FOTOACTO
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 42, 'Validar que el campo no este vacio', 'ADD', 'alumnograduacion_fotoacto_vacio_KO', 'Complete el campo'],
+    //esto es coherente con lo que pide el documento?
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 43, 'Validar que el campo tenga un nombre con solo alfabeticos sin acentos ni ñ ni espacios', 'ADD', 'alumnograduacion_fotoacto_nombrefoto_formato_KO', 'El formato deben ser alfabeticos sin acentos, ñ ni espacios.'],
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 44, 'Validar que el campo tenga un nombre con un minimo de 7 caracteres', 'ADD', 'alumnograduacion_fotoacto_nombrefoto_min_size_KO', 'El nombre debe contener minimo de 7 caracteres.'],
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 45, 'Validar que el campo tenga un nombre con un maximo de 100 caracteres', 'ADD', 'alumnograduacion_fotoacto_nombrefoto_max_size_KO', 'El nombre debe contener maximo de 100 caracteres.'],
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 46, 'Validar que el campo sea una imagen', 'ADD', 'alumnograduacion_fotoacto_formato_KO', 'El formato debe ser jpg.'],
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 47, 'Validar que el campo no supere el tamaño maximo de 2MB', 'ADD', 'alumnograduacion_fotoacto_max_size_KO', 'El tamaño máximo permitido es 2MB.'],
+    ['alumnograduacion', 'alumnograduacion_fotoacto', 48, 'Validar que el campo sea correcto', 'ADD', true, 'Correcto.'],   
 
 
 ];
